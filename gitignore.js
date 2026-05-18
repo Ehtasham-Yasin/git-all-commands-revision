4b382af24b51ac0be8ee41320e47218a39fd6c67
Ehtasham Yasin <ehtasham.yasin.dev@gmail.com>
2026-05-18 14:07:58 +0200

    restore

---
+++ gitignore.js
@@ -0,0 +1,21 @@
+console.log('This is a .gitignore file. It tells Git which files to ignore when committing changes to the repository.');
+// Example of a .gitignore file content:
+/*
+# Ignore node_modules folder
+node_modules/
+
+# Ignore log files
+*.log
+
+# Ignore environment variable files
+.env
+
+# Ignore build output
+dist/
+build/
+
+# Ignore IDE specific files
+.vscode/
+.idea/
+*/
+console.log('You can customize this file to ignore any files or folders that you do not want to be tracked by Git.');
